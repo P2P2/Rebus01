0cb582a1ea839bc4cf44d75529e6eef92b08a1a4
Paul Filipski <paul.filipski@gmail.com>
2019-02-16 12:41:05 -0500

    Hello world

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Handlers;
 
 namespace ConsoleApp1
 {
@@ -10,6 +16,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(localdb)\MSSQLLocalDb",
+                InitialCatalog = @"RebusTest",
+                IntegratedSecurity = true,
+            }.ToString();
+
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                activator.Register(() => new PrintDateTime());
+
+                var b = activator.Bus;
+
+                Configure.With(activator)
+                    .Transport(t => t.UseSqlServer(connectionString, "MyMessages"))
+                    .Start();
+
+                using (var timer = new Timer(_ => activator.Bus.SendLocal(DateTime.Now).Wait(), null, TimeSpan.FromSeconds(1.0), TimeSpan.FromSeconds(5.0)))
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public class PrintDateTime : IHandleMessages<DateTime>
+        {
+            public Task Handle(DateTime message)
+            {
+                Console.WriteLine($"\n{message:G}");
+                return Task.CompletedTask;
+            }
         }
     }
 }
